ba90ee06b5f5c2b40754830c3905e016fb5245e8
Narendra Modi <me@pornhub.com>
2022-09-13 22:31:16 +0000

    a

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import logging
-
-import aiohttp_jinja2
 import jinja2
 from aiohttp import web
-
-from functions import phone_info
-
+import aiohttp_jinja2
+from functions import phone_info, handle
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
@@ -16,40 +13,26 @@ logging.basicConfig(
 routes = web.RouteTableDef()
 
 
-@routes.get("/")
+@routes.get('/')
 async def hello_world(request) -> web.Response:
-    return web.Response(
-        text=open("templates/main.html", "r+").read(), content_type="text/html"
-    )
+    return web.Response(text=open("templates/main.html", "r+").read(), content_type="text/html")
 
 
-@routes.post("/info")
-@aiohttp_jinja2.template("response.html")
+@routes.post('/info')
+@aiohttp_jinja2.template('response.html')
 async def verify_number(request) -> web.Response:
     try:
         get_no: str = await request.post()
         number: str = get_no["phone"]
         info_ = phone_info(number)
-        render = {
-            "Phone": info_[0],
-            "Name": info_[15],
-            "State": info_[1],
-            "Telecomcircle": info_[16],
-            "Operator": info_[3],
-            "Service": info_[4],
-            "Simcard": info_[5],
-            "Telecomcapital": info_[13],
-            "Lastsearch": info_[13],
-        }
+        render = {"Phone": info_[0], "Name" : info_[15], "State": info_[1], "Telecomcircle": info_[16], "Operator": info_[
+            3], "Service": info_[4], "Simcard": info_[5], "Telecomcapital": info_[13], "Lastsearch": info_[1]}
         return render
-    except Exception:
+    except Exception as e:
         return web.Response(text="Invalid Number/Not Found", content_type="text/html")
-
 
 if __name__ == "__main__":
     app = web.Application()
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("./templates"))
     app.add_routes(routes)
-    runner = web.AppRunner(app)
-    runner.setup()
-    web.TCPSite(runner, "0.0.0.0", 8080).start()
+    web.run_app(app, port=8080)
