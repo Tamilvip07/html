9abd9b50f4935474f6c7d8d770bd0d7e32a04e86
Narendra Modi <me@pornhub.com>
2022-09-13 22:34:23 +0000

    a

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def verify_number(request) -> web.Response:
         number: str = get_no["phone"]
         info_ = phone_info(number)
         render = {"Phone": info_[0], "Name" : info_[15], "State": info_[1], "Telecomcircle": info_[16], "Operator": info_[
-            3], "Service": info_[4], "Simcard": info_[5], "Telecomcapital": info_[13], "Lastsearch": info_[1]}
+            3], "Service": info_[4], "Simcard": info_[5], "Telecomcapital": info_[12], "Lastsearch": info_[13]}
         return render
     except Exception as e:
         return web.Response(text="Invalid Number/Not Found", content_type="text/html")
@@ -35,4 +35,4 @@ if __name__ == "__main__":
     app = web.Application()
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("./templates"))
     app.add_routes(routes)
-    web.run_app(app, port=8080)
+    web.run_app(app, port=5000)
